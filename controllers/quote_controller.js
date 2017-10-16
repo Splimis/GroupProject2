a6cdfab2f449f2d322dc8cc8655ea63f92fe1226
User <splimis@gmail.com>
2017-10-16 11:02:03 -0400

    backend works

--- controllers/quote_controller.js
+++ controllers/quote_controller.js
@@ -26,7 +26,7 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
   quotations.insertOne([
-    'quote'
+    'quotation'
   ], [
     req.body.quotation
   ], function(data) {
