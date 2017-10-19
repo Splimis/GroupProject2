7ff5f2f8704d1913336c8e43bfa4617bdf093aee
User <splimis@gmail.com>
2017-10-19 11:21:54 -0400

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,17 @@
 INSERT INTO quote (quotation, CharID, url) VALUES ('Listen Morty, I hate to break it to you, but what people calls "love" is just a chemical reaction that compels animals to breed. It hits hard, Morty, then it slowly fades, leaving you stranded in a failing marriage. I did it. Your parents are gonna do it. Break the cycle, Morty. Rise above. Focus on science.', 'Rick', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+
   src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/rickquote1?in=jose-arancibia-4749278/sets/quote-generator&amp;auto_play=true">
 </iframe>' );
 INSERT INTO quote (quotation, CharID, url) VALUES ('Turn myself into a pickle Morty, boom, big reveal, Im a pickle. What do you think about that? I turned myself into a pickle. Im pickle Riiick', 'Rick', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+
   src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/rickquote2?in=jose-arancibia-4749278/sets/quote-generator&amp;auto_play=true">
 </iframe>' );
 INSERT INTO quote (quotation, CharID, url) VALUES ('What is my purpose? You pass butter. Oh my god. Yeah welcome to the club.', 'Rick', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+
   src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/rickquote3?in=jose-arancibia-4749278/sets/quote-generator&amp;auto_play=true">
 </iframe>' );
 INSERT INTO quote (quotation, CharID, url) VALUES ('Would you like to RSVP or send a gift? No. wedding are like funerals with cake. If I wanted to watch someone throw their life away Id hang out with Jerry all day. Affirmative, returning to sender.', 'Rick', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+
   src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/rickquote4?in=jose-arancibia-4749278/sets/quote-generator&amp;auto_play=true">
 </iframe>' );
 INSERT INTO quote (quotation, CharID, url) VALUES ('What about the reality we left behind? What about the reality where Hitler cured cancer, Morty? The answer is still dont think about it', 'Rick', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
@@ -19,33 +23,42 @@ INSERT INTO quote (quotation, CharID, url) VALUES ('On one of our adventures, Ri
 INSERT INTO quote (quotation, CharID, url) VALUES ('Get your shit together, get it all together and put it in a back pack, all your shit, so its together. And if you gotta take it some where, take it somewhere, you know, take it to the shit store and sell it, or put it in the shit museum. I dont care what you do, you just gotta get it together.', 'Morty', '<iframe width="0" height="0" scrolling="no" frameborder="no"
   src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/mortyquote2?in=jose-arancibia-4749278/sets/quote-generator&amp;auto_play=true">
 </iframe>' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote8', 'Morty', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote9', 'Morty', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote10', 'Morty', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote11', 'Beth', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote12', 'Beth', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote13', 'Beth', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote14', 'Beth', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote15', 'Beth', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote16', 'Jerry', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote17', 'Jerry', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote18', 'Jerry', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote19', 'Jerry', 'url' );
-INSERT INTO quote (quotation, CharID, url) VALUES ('insert rick and morty quote20', 'Jerry', 'url' );
-
-
-
-"https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/mortyquote3&amp;auto_play=true"
-
-
-
-https://soundcloud.com/jose-arancibia-4749278/mortyquote5&amp;auto_play=true
-
-<iframe width="0%" height="0" scrolling="no" frameborder="no"
-  src="https://w.soundcloud.com/player/?url=">
-
-https://soundcloud.com/jose-arancibia-4749278/rickquote2?in=jose-arancibia-4749278/sets/quote-generator&amp;auto_play=true
-
-<iframe width="0%" height="0" scrolling="no" frameborder="no"
+INSERT INTO quote (quotation, CharID, url) VALUES ('Rick, thats it, Im done with these insane adventures. That was really traumatizing. I quit. Im out.', 'Morty', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/mortyquote3&amp;auto_play=true">
+</iframe>');
+INSERT INTO quote (quotation, CharID, url) VALUES ('He bails on everybody! He bailed on mom when she was a kid! He-he bailed on tiny planet! And in case I never made this clear to you Summer, he bailed on you. He left you to rot in a world that he ruined because he doesn’t care because nobody’s special to him Summer! Not even himself. So if you really want your grandpa back, grab a shovel. The one that won’t let you down is buried in your backyard! Summer: You’re right!
+Morty: Wh-what?! I’m not right! I was using ghoulish overkill! Ghoulish overkill Summer!', 'Morty', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/mortyquote4&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Morty: You guys hit the baskets, I’ll disarm the drunkenly improvised neutrino-bomb. There’s a 40% chance it’s a dud, but you should still stay back. Rick: How many of these – Morty: TOO MANY RICK, TOO MANY.', 'Morty', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
   src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/mortyquote5&amp;auto_play=true">
-</iframe>
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Beth:Tommy? Tommy?! This is a nightmare! I can’t believe you used to lock me up in this glorified chicken coop. Rick: Chicken Coop? Those are procedurally generated clouds, Beth. That River is a rainbow, literally. Come on, I-I-I put real elbow grease into this place. Beth: You’re supposed to put elbow grease into your daughter.', 'Beth', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/bethquote1&amp;auto_play=true"></iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('I’m one of infinite Beth’s with infinite Fathers in infinite universes. It’s called a hug, Dad. It wont kill you!', 'Beth', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/bethquote2&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Photos! Hard evidence! You’re not in any of my photos Mr. Poopy Butthole.', 'Beth', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/bethquote3&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Beth: WHOA, WHAT THE HELL?
+Jerry: Look out for that homeless guy!
+Beth: Open the door. Open the door! OPEN THE DOOR!
+Jerry: There isn’t time just run!
+Beth: Get out here and help me!
+Jerry: They say you shouldn’t do that. Just run!', 'Beth', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/bethquote4&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Loving you is work, Jerry. Hard. Work. Like building a homeless shelter. Nobody wants to say no to doing it, but some people put the work in. So what do you say? Do you see me working here? Does this work seem tedious to me?', 'Beth', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/bethquote5&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Try to relax. Have you ever tried to relax? It is a paradox.', 'Jerry', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/jerryquote1&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Innocent people are going to die because of me. Why am I so mediocre?', 'Jerry', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/jerryquote2&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('Well, I remember feeling that way about a young lady named “your mom” and that’s not an urban diss. Your mom was my Jessica. I remember the first time I saw her.', 'Jerry', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
+  src="https://w.soundcloud.com/player/?url=https://soundcloud.com/jose-arancibia-4749278/jerryquote3&amp;auto_play=true">
+</iframe>' );
+INSERT INTO quote (quotation, CharID, url) VALUES ('I tell ya the galactic federation taking over earth – best thing that’s ever happened for this family! I just got my sixth promotion this week and I still don’t know what I do!', 'Jerry', '<iframe width="0%" height="0" scrolling="no" frameborder="no"
