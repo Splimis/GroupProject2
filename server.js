89682c87663ee17240d802576050b3b870712d74
Splimis <splimis@gmail.com>
2017-11-18 17:57:21 -0500

    connected to mongodb and dependencies added to construct authentication

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var exphbs = require('express-handlebars');
 var popper = require('popper');
+var passport = require('passport');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var LocalStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
 
 var app = express();
 var port = process.env.PORT || 3000;
