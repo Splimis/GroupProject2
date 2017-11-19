1d4020e08687f18d6f7c8d02808531750120afe8
Splimis <splimis@gmail.com>
2017-11-19 16:22:02 -0500

    admin only page now running securely

--- authRoutes/ensure.js
+++ authRoutes/ensure.js
@@ -4,14 +4,17 @@ var router = express.Router();
 
 
 //admin
-router.get('/admin', function(req, res) {
+router.get('/admin', ensureAuthenticated, function(req, res) {
 	res.render('admin');
 });
 
+//ensuring the user trying to access the admin page is actually logged in.
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
+		// console.log("this compiles");
 		return next();
 	} else {
+		// console.log("this also compiles");
 		req.flash('error_msg', 'You are not logged in');
 		res.redirect('/');
 	}
--- authRoutes/users.js
+++ authRoutes/users.js
@@ -78,7 +78,7 @@ passport.deserializeUser(function(id, done) {
 });
 
 router.post('/login',
-	passport.authenticate('local', {successRedirect: '/ensure/admin', failureRedirect:'/', failureFlash: true}),
+	passport.authenticate('local', {successRedirect: '/ensure/admin', failureRedirect:'/users/login', failureFlash: true}),
 	function(req, res) {
 		res.redirect('/');
 
