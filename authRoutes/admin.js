316008ed2ca09bf60ba3bbf9595c06953153942e
Splimis <splimis@gmail.com>
2017-11-19 13:51:28 -0500

    figuring out authentication ensurance

--- authRoutes/admin.js
+++ authRoutes/admin.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-//Get Login Page
-router.get('/admin', function(req, res) {
+router.get('/admin', ensureAuthenticated, function(req, res) {
 	res.render('admin');
-})
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		req.flash('error_msg', 'You are not logged in');
+		res.redirect('/');
+	}
+}
 
 module.exports = router;
